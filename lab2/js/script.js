ecfccc17836c374ab88e8b9fe3deaa00b2fb6cf7
Justin <justin.andreoli04@gmail.com>
2026-02-15 21:26:52 -0800

    commit before hw submit

--- lab2/js/script.js
+++ lab2/js/script.js
@@ -1,18 +1,26 @@
+let wins = 0;
+let losses = 0;
+let randomNumber;
+let guessCount = 0;
+
 //Event listener
 document.querySelector("#guessButton").addEventListener("click", guess);
+document.querySelector("#resetBtn").addEventListener("click", initializeGame);
+
 let userGuess = document.querySelector("#numberInput").value;
 // value is only for input elements
 
-//Global variable
-let randomNumber = Math.floor(Math.random() * 99) + 1;
-console.log(randomNumber);
+initializeGame();
 
-let guessCount = 0;
 
 function guess(){
     let userGuess = document.querySelector("#numberInput").value;
     // alert(userGuess);
     // document.querySelector("#userGuesses").textContent += userGuess + " ";
+    if (userGuess > 99 || userGuess < 1){
+        alert("Guess a number between 1 and 99");
+        return;
+    }
     guessCount++;
     document.querySelector("#userGuesses").textContent += `${userGuess} `;
 
@@ -25,8 +33,33 @@ function guess(){
         document.querySelector("#hint").style.color = "blue";
         document.querySelector("#hint").textContent = `Too Low`;
     }else if (guessCount<= 7){
-        document.querySelector("#hint").textContent = `CONGRATS! Guessed in less than 7 tries.`;
-    }else{
-        document.querySelector("#hint").textContent = `Guessed Correct!`;
+        document.querySelector("#hint").textContent = `CONGRATS! Guessed the number in ${guessCount} tries`;
+        wins++;
+        endGame();
+        return;
     }
+    if(guessCount >= 7){
+        document.querySelector("#hint").textContent = `You lost`;
+        document.querySelector("#hint").style.color = "red";
+        losses--;
+        endGame();
+    }
+}
+
+function initializeGame(){
+    guessCount = 0;
+    randomNumber = Math.floor(Math.random() * 99) + 1;
+    console.log(randomNumber);
+    document.querySelector("#resetBtn").style.display = "none";
+    document.querySelector("#guessButton").style.display = "inline";
+
+    document.querySelector("#wins").textContent = `Wins: ${wins}`;
+    document.querySelector("#losses").textContent = `Losses: ${losses}`;
+
+    document.querySelector("#userGuesses").textContent = "";
+}
+
+function endGame(){
+    document.querySelector("#guessButton").style.display = "none";
+    document.querySelector("#resetBtn").style.display = "inline";
 }
